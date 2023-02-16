377e1aa40366ae41e5c301549a8c160a311447de
Vinicius Teixeira Saraiva <vinicius.ts.online@gmail.com>
2023-02-16 17:09:55 -0300

    feat: implemented routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule }        from '@angular/core';
 import { HttpClientModule} from "@angular/common/http";
 import { BrowserModule }   from '@angular/platform-browser';
+import { RouterModule }     from "@angular/router";
+
+import { ROUTES } from "./app.routes";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +25,8 @@ import { DiversaoComponent } from './diversao/diversao.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -1,6 +1,6 @@
-import { Oferta } from './shared/oferta.model';
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import {Oferta}     from './shared/oferta.model';
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
 
 import 'rxjs';
 
@@ -8,13 +8,22 @@ import 'rxjs';
 export class OfertasService {
   URL = 'http://localhost:3000/ofertas?destaque=true';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
 
   public getOfertas(): Promise<Oferta[] | undefined> {
-    return this.http.get<Oferta[]>(this.URL).toPromise()
+    return this.http.get<Oferta[]>(this.URL)
+      .toPromise()
       .then(function (resposta) {
         console.log(resposta);
         return resposta;
       });
+  }
+
+  public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
+    return this.http.get<Oferta[]>(
+      `http://localhost:3000/ofertas?categoria=${categoria}`)
+      .toPromise()
+      .then()
   }
 }
